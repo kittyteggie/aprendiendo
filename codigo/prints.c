4cb4ac2fef6376ba7f7dfbc337a72525002a3ab7
Kitty <4lessandro@proton.me>
2026-05-16 23:04:38 +0000

    Create prints.c
    Primer parte del curso, aprender a como utilizar los prints y como optimizar codigo basico

---
+++ codigo/prints.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+int main(){
+	printf("Hola mundo!\n");
+	printf("Segunda linea\n");
+	return 0;
+}
+*/
+// version optimizada
+int main(){printf("Hola mundo!\n");printf("Segunda linea!");return 0;}
