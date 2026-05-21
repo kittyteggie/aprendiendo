3f1ff664cd0c3e5fab80b2c1cc61dd6cd43a2073
Kitty <4lessandro@proton.me>
2026-05-21 01:14:32 +0000

    Update 03-radio-y-area.c
    Esta fue mi favorita, es simple y no es la tipica calculadora de 1 + 1 = hola mundo

--- ejercicios/03-radio-y-area.c
+++ ejercicios/03-radio-y-area.c
@@ -5,14 +5,20 @@ int main()
 {
 	double radio = 0.0;
 	double area = 0.0;
+	double superficieDelArea = 0.0;
+	double volumen = 0.0;
 	const double PI = 3.141592653589793;
 
 	printf("Pon el radio: ");
 	scanf("%lf", &radio);
 
 	area = PI * pow(radio, 2);
+	superficieDelArea = 4 * PI * pow(radio, 2);
+	volumen = (4.0 / 3.0) * PI * pow(radio, 3);
 
-	printf("Area: %lf", area);
+	printf("Area: %lf\n", area);
+	printf("Superficie del Area: %lf\n", superficieDelArea);
+	printf("Volumen: %lf\n", volumen);
 
 	return 0;
 }
