0336727be02830d05c8e2e639c8762ce386aebc9
Kitty <4lessandro@proton.me>
2026-05-21 01:41:06 +0000

    Create 07-if.c
    Lo terminare mañana o mas tarde

---
+++ codigo/07-if.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+	int edad = 0;
+
+	printf("Pon tu edad: ");
+	scanf("%d", &edad);
+
+	if(edad >= 18){
+		printf("Eres un adulto\n");
+	}
+	else if(edad == 0){
+		printf("Onta bebe?\n");
+	}else if(edad < 0){
+		printf("Aun no has nacido, sigues siendo un fetito\n");
+	}else{
+		printf("Eres un menor\n");
+	}
+
+	return 0;
+}
