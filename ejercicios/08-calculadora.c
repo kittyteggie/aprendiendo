a22a715f20304709b75150485ae10bf21e662a6b
Kitty <4lessandro@proton.me>
2026-05-22 21:45:15 +0000

    Create 08-calculadora.c
    Es curioso que nos pusieran ahora a hacer una calculadora normal cuando hace rato era todo calculadoras de otro tipo

---
+++ ejercicios/08-calculadora.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+	
+	char operador = '\0';
+	double num1 = 0.0;
+	double num2 = 0.0;
+	double resultado = 0.0;
+	bool error = 0;
+	char *error2 = "";
+
+	printf("Coloque el primer numero: ");
+	scanf("%lf", &num1);
+
+	printf("Coloque el operador (+ - * /): ");
+	scanf(" %c", &operador);
+
+	printf("Coloque el segundo numero: ");
+	scanf("%lf", &num2);
+
+	switch(operador){
+		case '+':
+			resultado = num1 + num2;
+			break;
+		case '-':
+			resultado = num1 - num2;
+			break;
+		case '*':
+			resultado = num1 * num2;
+			break;
+		case '/':
+			if(num2 == 0){
+				error = 1;
+				error2 = "No puedes dividir entre cero\n";
+			}
+			else{
+				resultado = num1 / num2;
+			}
+	}
+
+	if(error){
+		printf("ERROR: %s", error2);
+	}else{
+		printf("Resultado: %.3lf\n", resultado);
+	}
+
+	return 0;
+}
