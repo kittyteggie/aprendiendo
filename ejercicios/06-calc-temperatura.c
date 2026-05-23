13c771465317b77044affa9c5b739e70da0858a4
Kitty <4lessandro@proton.me>
2026-05-23 22:36:41 +0000

    Update 06-calc-temperatura.c
    error ortografico

--- ejercicios/06-calc-temperatura.c
+++ ejercicios/06-calc-temperatura.c
@@ -4,27 +4,27 @@ int main(){
 	
 	char opcion = '\0';
 	float fahrenheit = 0.0f;
-	float celcios = 0.0f;
+	float celcius = 0.0f;
 
 	printf("Calculadora de temperatura\n");
 	printf("--------------------------\n");
 
-	printf("C. Celcios a Fahrenheit\n");
-	printf("F. Fahrenheit a Celcios\n");
+	printf("C. Celcius a Fahrenheit\n");
+	printf("F. Fahrenheit a Celcius\n");
 
 	printf("Escoje una opción (C/F): ");
 	scanf(" %c", &opcion);
 
 	if(opcion == 'C'){
-		printf("Coloque la temperatura en Celcios: ");
-		scanf("%f", &celcios);
-		fahrenheit = (celcios * 9 / 5) + 32;
-		printf("%.2f grados celcios es igual a %.2f Fahrenheit\n", celcios, fahrenheit);
+		printf("Coloque la temperatura en Celcius: ");
+		scanf("%f", &celcius);
+		fahrenheit = (celcius * 9 / 5) + 32;
+		printf("%.2f grados celcius es igual a %.2f Fahrenheit\n", celcius, fahrenheit);
 	}else if(opcion == 'F'){
 		printf("Coloque la temperatura en Fahrenheit: ");
 		scanf("%f", &fahrenheit);
-		celcios = (fahrenheit - 32) * 5 / 9;
-		printf("%.2f Fahrenheit es igual a %.2f grados celcios\n", fahrenheit, celcios);
+		celcius = (fahrenheit - 32) * 5 / 9;
+		printf("%.2f Fahrenheit es igual a %.2f grados celcius\n", fahrenheit, celcius);
 	}else{
 		printf("ERROR: Opción invalida, por favor ponga C o F en el input\n");
 	}
