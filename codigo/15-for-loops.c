8b68ead11e28eb75c940beb321d6c50bb1519b08
Kitty <4lessandro@proton.me>
2026-05-25 20:10:19 -0300

    Update 15-for-loops.c

--- codigo/15-for-loops.c
+++ codigo/15-for-loops.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
+// Si usas Windows reemplaza esto por #include <windows.h>
 
 int main(){
 
 	for(int i = 10; int i >= 10; i--){
-		sleep(1);
+		sleep(1); // Si usas Windows reemplaza esto por Sleep(1000);
 		printf("%d\n", i);
 	}
 
