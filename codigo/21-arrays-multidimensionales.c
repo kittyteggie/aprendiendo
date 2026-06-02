d9056a9e759de1b1361fdb56f1528cc6676f4423
Kitty <4lessandro@proton.me>
2026-06-02 22:39:02 +0000

    Update 21-arrays-multidimensionales.c

--- codigo/21-arrays-multidimensionales.c
+++ codigo/21-arrays-multidimensionales.c
@@ -2,14 +2,11 @@
 
 int main(){
 
-  int numeros[][3] = {{1, 2, 3},
-                      {4, 5, 6},
-                      {7, 8, 9},
-                      {10, 11, 12}};
+  char numpad[][3] = {{'1', '2', '3'},{'4', '5', '6'},{'7', '8', '9'},{'*', '0', '#'}};
 
   for(int i = 0; i < 4; i++){
     for(int j = 0; j < 3; j++){
-      printf("%d ", numeros[i][j]);
+      printf("%c ", numpad[i][j]);
     }
   }
   printf("\n");
