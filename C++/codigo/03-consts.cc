07b090d3cde6faa45cbf6919b0bbd7e407e39722
Kitty <4lessandro@proton.me>
2026-06-07 19:25:27 +0000

    Create 03-consts.cc

---
+++ C++/codigo/03-consts.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+  const double PI = 3.14159;
+  double radio = 10;
+  const double CIRCUNFERENCIA = 2 * PI * radio;
+  
+  std::cout << CIRCUNFERENCIA << "cm";
+  return 0;
+}
